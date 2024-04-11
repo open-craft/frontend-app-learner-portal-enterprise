3490d8f6f00dc761fd9df5b62042b528a1bc40ab
Arunmozhi <arunmozhi@opencraft.com>
2024-04-11 14:57:32 +1000

    temp: removed edX from the integration warning modal copy

--- src/components/integration-warning-modal/ModalBody.jsx
+++ src/components/integration-warning-modal/ModalBody.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ModalBody = () => (
   <div>
     <p className="m-0">
-      You are viewing only the edX courses that your organization has made available to you.
+      You are viewing only the courses that your organization has made available to you.
       There may be other learning resources from different sources available in your learning
       management system.
     </p>
